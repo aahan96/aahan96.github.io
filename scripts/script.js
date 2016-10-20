716305c0251d80baa3724842535fde9119859c24
aahan96 <axs149430@utdallas.edu>
2016-10-20 15:34:43 -0500

    Many changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 setTimeout(function(){
         $(".heading").typed({
-            strings: ["I like to code", "I like to read", "I like to program websites"],
+            strings: ["I like to code.", "I like to read.", "I like to create websites.", "Angels & Demons is my favorite book.", "I prefer C++ over C", "Java is my favorite language", "Computers make very fast, very accurate mistakes.", "Artificial intelligence usually beats real stupidity.", "If at first you don’t succeed; call it version 1.0.", "My software never has bugs. It just develops random features.", "Programmers are tools for converting caffeine into code."],
             typeSpeed: 50, // typing speed
             backDelay: 750, // pause before backspacing
             loop: true, // loop on or off (true or false)
