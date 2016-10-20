7f823a2a44bd4078fb2b38fa04c519962f74135c
aahan96 <axs149430@utdallas.edu>
2016-10-20 15:43:22 -0500

    Changed strings

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,14 @@
 setTimeout(function(){
         $(".heading").typed({
-            strings: ["I like to code.", "I like to read.", "I like to create websites.", "Angels & Demons is my favorite book.", "I prefer C++ over C", "Java is my favorite language", "Computers make very fast, very accurate mistakes.", "Artificial intelligence usually beats real stupidity.", "If at first you don’t succeed; call it version 1.0.", "My software never has bugs. It just develops random features.", "Programmers are tools for converting caffeine into code."],
-            typeSpeed: 50, // typing speed
+            strings: ["I like to code.", "I like to read.", "I like to create websites.", "I prefer C++ over C", "Java is my favorite language", "Computers make very fast, very accurate mistakes.", "Artificial intelligence usually beats real stupidity.", "If at first you don’t succeed; call it version 1.0.", "My software never has bugs. It just develops random features.", "Programmers are tools for converting caffeine into code."],
+            typeSpeed: 30, // typing speed
             backDelay: 750, // pause before backspacing
             loop: true, // loop on or off (true or false)
             loopCount: false, // number of loops, false = infinite
             callback: function(){ } // call function after typing is done
         });
     }, 0);
+
+
 
 
